883621346129497e8e3349f0df3f7b4cc6905da1
Felipe Celeri <felipeceleri@gmail.com>
2022-07-21 10:55:15 -0300

    ajustes

---
+++ app/Views/inicio.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="pt-Br">
+
+<head>
+    <title>Psicologa | Carolina Celeri</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        /* Style the video: 100% width and height to cover the entire window */
+        #myVideo {
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            min-width: 100%;
+            min-height: 100%;
+            z-index: -1;
+        }
+
+        /* Add some content at the bottom of the video/page */
+        .content {
+            position: fixed;
+            bottom: 0;
+            background: rgba(0, 0, 0, 0.5);
+            color: #f1f1f1;
+            width: 100%;
+            padding: 20px;
+        }
+
+        /* Style the button used to pause/play the video */
+        #myBtn {
+            width: 200px;
+            font-size: 18px;
+            padding: 10px;
+            border: none;
+            background: #000;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        #myBtn:hover {
+            background: #ddd;
+            color: black;
+        }
+
+        .jumbotron {
+            background-color: rgba(255, 255, 255, 0.98);
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- The video -->
+    <video autoplay muted loop id="myVideo">
+        <source src="public/fundo.mp4" type="video/mp4">
+    </video>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Precisando de ajuda?</h1>
+            <p class="lead">Em muitos casos uma escuta ativa pode ser a solução.</p>
+            <hr class="my-4">
+            <p>Caso seja o que precise no momento, não hesite em nos contatar pelo botão abaixo. <br>Você será redirecionado ao contato whatsapp de um profissional habilitado com formação adequada e anos de experiencia. </p>
+            <br> <br><br><br>
+            <p class="lead text-center">
+                <a class="btn btn-primary btn-lg btn-warning" href="#" role="button">Me ajude!</a>
+            </p>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
